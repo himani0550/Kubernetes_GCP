5878b7c1333e1d5b3f5ac6b51ec833eee183a5f5
himani0550 <himani0550@gmail.com>
2024-03-09 22:10:49 +0530

    add

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const REACT_APP_API_URL=process.env.REACT_APP_API_URL;
+const REACT_APP_API_HELLO=process.env.REACT_APP_API_HELLO;
+
+function Home() {
+  const [message, setMessage] = useState("");
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(REACT_APP_API_HELLO)
+      .then((response) => {
+        setMessage(response.data.msg);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(REACT_APP_API_URL)
+      .then((response) => {
+        setProfile(response.data);
+        
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  },[]);
+
+  
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      <div>
+        <h2>Profile</h2>
+        {
+        profile.map((user) => {
+            console.log('user', user)
+          return (
+            <div>
+              <h3>Name: {user.name}</h3>
+              <h3>Age: {user.age}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
